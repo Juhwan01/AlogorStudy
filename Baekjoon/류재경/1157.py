556c56acdaba6e2490e64c4864e92d6f3f49757a
Ryu <fbworud123456@naver.com>
2025-03-19 19:32:04 +0900

    1157 단어공부

--- Baekjoon/류재경/1157.py
+++ Baekjoon/류재경/1157.py
@@ -1,7 +1,20 @@
 s = input().upper()
-cnt = 0
-for i in range(len(s)):
-    print(s.count)
+s_set = list(set(s))
+cnt = []
+
+for i in s_set:
+    cnt.append(s.count(i))
+    
+if cnt.count(max(cnt)) > 1:
+    print("?")
+else:
+    print(s_set[cnt.index(max(cnt))])
+     
+    
+
+    
+
+    
     
     
    
