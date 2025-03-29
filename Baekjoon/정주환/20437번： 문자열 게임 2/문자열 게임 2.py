56833f89847a3b06c7b7b68448d05cc9d95c6e24
Juhwan01 <wnghks5432@naver.com>
2025-03-29 15:12:31 +0900

    20437

--- Baekjoon/정주환/20437번： 문자열 게임 2/문자열 게임 2.py
+++ Baekjoon/정주환/20437번： 문자열 게임 2/문자열 게임 2.py
@@ -1,16 +1,28 @@
+from collections import defaultdict
 import sys
+
 input = sys.stdin.readline
 
-valid = False
-n = int(input())
-for i in range(n):
-    result = []
-    problem = input().strip()
-    contain = int(input())
-    for alpha in problem:
-        if problem.count(alpha) >= contain and alpha not in result:
-            result.append(alpha)
-    
-
-
-  
+t = int(input())
+for _ in range(t):
+    string = input().rstrip()
+    n = int(input())
+    word = defaultdict(list)
+    for i in range(len(string)):
+        if string.count(string[i]) >= n:
+            word[string[i]].append(i)
+    if len(word) == 0:
+        print(-1)
+    else:
+        min_value = float('inf')
+        max_value = float('-inf')
+        for index in word:
+            word_index=word[index]
+            for i in range(len(word_index)):
+                try:
+                    length = word_index[i+n-1] - word_index[i]
+                    min_value = min(min_value, length)
+                    max_value = max(max_value, length)
+                except:
+                    continue
+        print(min_value+1, max_value+1)
