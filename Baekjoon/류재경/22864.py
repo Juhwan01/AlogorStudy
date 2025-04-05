d76308abf226a8f17ba34f036888c4ed639c3bea
Ryu <fbworud123456@naver.com>
2025-04-05 21:39:16 +0900

    22864

--- Baekjoon/류재경/22864.py
+++ Baekjoon/류재경/22864.py
@@ -1,3 +1,21 @@
 import sys
 input = sys.stdin.readline
 
+x = list(map(int, input().split()))
+M = 0
+B = 0
+H = 0
+
+while H < 24:
+    if M + x[0] > x[3]:
+        M -= x[2]
+        if M < 0:
+            M = 0
+    else:
+        B += x[1]
+        M += x[0]
+    if M > x[3]:
+        break
+    H += 1
+
+print(B)
