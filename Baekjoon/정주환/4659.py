9a505b97621e57e1b42768b2328076a8232b60c2
Juhwan01 <wnghks5432@naver.com>
2025-04-06 19:21:16 +0900

    4659

--- Baekjoon/정주환/4659.py
+++ Baekjoon/정주환/4659.py
@@ -1,39 +1,32 @@
 import sys
 input = sys.stdin.readline
 
-vowel = ['a','e','i','o','u']
+vowels = ['a', 'e', 'i', 'o', 'u']
 
 while True:
     string = input().rstrip()
-    flag = True
+    
     if string == "end":
         break
-    if not any(v in string for v in vowel):
-        print("<{}> is not acceptable.".format(string))
+    
+    flag = True
+    
+    if not any(v in string for v in vowels):
+        flag = False
+    
+    if flag:
+        for i in range(len(string) - 2):
+            if all(string[i+j] in vowels for j in range(3)):
+                flag = False
+            elif all(string[i+j] not in vowels for j in range(3)):
+                flag = False
+    
+    if flag:
+        for i in range(len(string) - 1):
+            if string[i] == string[i+1] and string[i] not in ['e', 'o']:
+                flag = False
+                break
+    if flag:
+        print("<{}> is acceptable.".format(string))
     else:
-        for i in range(len(string)):
-            try:
-                tmp1 = string[i:i+3]
-                tmp2 = string[i:i+2]
-                sum_vowel=sum(tmp1.count(v)for v in vowel)
-                sum_alpha=sum(tmp2.count(t)for t in tmp2)
-                if sum_vowel == 3 or sum_vowel == 0:
-                    print("<{}> is not acceptable.".format(string))
-                    flag=False
-                    break
-                elif sum_alpha == 2 and tmp2 != "ee" and tmp2 != "oo":
-                    print("<{}> is not acceptable.".format(string))
-                    flag=False
-                    break
-            except:
-                continue
-        if flag:
-            print("<{}> is acceptable.".format(string))
-            
-        
-        
-
-    
-    
-    
-        
+        print("<{}> is not acceptable.".format(string))
