bd32562d8cf2c4e1e5a34442bfaaca99b13633d7
Juhwan01 <wnghks5432@naver.com>
2025-04-29 20:27:12 +0900

    14503

--- Baekjoon/정주환/14503.py
+++ Baekjoon/정주환/14503.py
@@ -8,14 +8,14 @@ house=[]
 flag=False
 
 def find(r,c):
-    start_row = 0 if r-4<0 else r-4
-    start_col = 0 if c-4<0 else c-4
-    end_row = n if r+5>n else r+5
-    end_col = m if c+5>m else c+5
-    for i in range(start_row,end_row):
-        for j in range(start_col,end_col):
-            if house[i][j] == 0:
-                return True
+    dr = [-1, 0, 1, 0]
+    dc = [0, 1, 0, -1]
+    
+    for i in range(4):
+        nr = r + dr[i]
+        nc = c + dc[i]
+        if 0 <= nr < n and 0 <= nc < m and house[nr][nc] == 0:
+            return True
     return False
 
 for i in range(n):
@@ -29,11 +29,11 @@ while True:
     if find(r,c):
         d = 3 if d-1<0 else d-1
         if d==0:
-            r = r-1 if house[r][r-1]==0 else r
+            r = r-1 if house[r-1][c]==0 else r
         elif d==1:
             c =  c+1 if house[r][c+1]==0 else c
         elif d==2:
-            r = r+1 if house[r][r+1]==0 else r
+            r = r+1 if house[r+1][c]==0 else r
         elif d==3:
             c = c-1 if house[r][c-1]==0 else c
     else:
