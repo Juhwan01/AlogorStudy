276bab018f1f9614e1d3a13490fd647804a35c60
Juhwan01 <wnghks5432@naver.com>
2025-05-14 09:29:19 +0900

    19941

--- Baekjoon/정주환/19941.py
+++ Baekjoon/정주환/19941.py
@@ -5,13 +5,6 @@ input = sys.stdin.readline
 n,k = map(int,input().strip().split())
 
 string = input()
-events=defaultdict(list)
 
-for i in range(len(string)):
-    if string[i] == "P":
-        events["P"].append(i)
-    else:
-        events["H"].append(i)
-
-print(events)
+def dfs()
             
