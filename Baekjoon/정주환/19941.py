74b69e5c68d39c9fbb14176f350d9c57fd388298
Juhwan01 <wnghks5432@naver.com>
2025-05-14 17:42:09 +0900

    19941

--- Baekjoon/정주환/19941.py
+++ Baekjoon/정주환/19941.py
@@ -6,5 +6,29 @@ n,k = map(int,input().strip().split())
 
 string = input()
 
-def dfs()
-            
+p_index = []
+h_index = []
+
+for i in range(n):
+    if string[i] == "H":
+        h_index.append(i)
+    else:
+        p_index.append(i)
+        
+cnt=0
+index=0
+cnt_h=len(h_index)
+for p in p_index:
+    while index<cnt_h:
+        h=h_index[index]
+        if abs(p-h)<=k:
+            cnt+=1
+            index+=1
+            break
+        elif p<h:
+            break
+        else:
+            index+=1
+        
+        
+print(cnt)
